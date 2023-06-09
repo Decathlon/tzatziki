7da195e8d7bfb786a6dead9c8467da69b494a801
alexandre patelli <alexandre.patelli@decathlon.com>
2023-06-09 09:59:15 +0200

    fix import

--- tzatziki-jackson/src/main/java/com/decathlon/tzatziki/utils/JacksonMapper.java
+++ tzatziki-jackson/src/main/java/com/decathlon/tzatziki/utils/JacksonMapper.java
@@ -1,7 +1,6 @@
 package com.decathlon.tzatziki.utils;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
