28e21288c726d58948657eb7465d7bf6b9fecd0b
alexandre patelli <alexandre.patelli@decathlon.com>
2024-04-26 18:10:20 +0200

    refresh branch + add first impl of new browser methods

--- tzatziki-front/src/main/java/com/decathlon/tzatziki/front/integration/Browser.java
+++ tzatziki-front/src/main/java/com/decathlon/tzatziki/front/integration/Browser.java
@@ -5,12 +5,21 @@ import com.decathlon.tzatziki.front.interactions.models.HTMLElement;
 
 import java.util.List;
 
+/**
+ * selector input parameter can be of type CSS or XPath
+ */
 public interface Browser {
     void get(String page);
 
-    List<HTMLElement> find(String cssSelector);
+    List<HTMLElement> find(String selector);
 
-    default List<HTMLElement> actionOn(String cssSelector, Action action, String... params) {
-        return this.find(cssSelector).stream().map(htmlElement -> htmlElement.performAction(action, params)).toList();
+    default List<HTMLElement> actionOn(String selector, Action action, String... params) {
+        return this.find(selector).stream().map(htmlElement -> htmlElement.performAction(action, params)).toList();
     }
+
+    boolean waitForElement(String selector, Boolean isVisible, Integer timeoutMs);
+
+    boolean waitForPage(String page, Integer timeoutMs);
+
+    void reload();
 }
--- tzatziki-front/tzatziki-playwright/src/main/java/com/decathlon/tzatziki/front/playwright/integration/PlaywrightBrowser.java
+++ tzatziki-front/tzatziki-playwright/src/main/java/com/decathlon/tzatziki/front/playwright/integration/PlaywrightBrowser.java
@@ -4,13 +4,16 @@ import com.decathlon.tzatziki.front.integration.Browser;
 import com.decathlon.tzatziki.front.interactions.models.HTMLElement;
 import com.decathlon.tzatziki.front.playwright.interactions.models.PlaywrightHTMLElement;
 import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.TimeoutError;
+import com.microsoft.playwright.options.WaitForSelectorState;
 
 import java.util.List;
 
 public class PlaywrightBrowser implements Browser {
-    private com.microsoft.playwright.Browser playwright;
+    private final com.microsoft.playwright.Browser playwright;
     private Page currentPage;
 
     public PlaywrightBrowser() {
@@ -33,11 +36,38 @@ public class PlaywrightBrowser implements Browser {
     }
 
     @Override
-    public List<HTMLElement> find(String cssSelector) {
+    public List<HTMLElement> find(String selector) {
         if(currentPage == null){
             throw new IllegalStateException("You must navigate to a page before trying to find elements");
         }
 
-        return currentPage.locator(cssSelector).all().stream().map(PlaywrightHTMLElement::new).map(HTMLElement.class::cast).toList();
+        return currentPage.locator(selector).all().stream().map(PlaywrightHTMLElement::new).map(HTMLElement.class::cast).toList();
+    }
+
+    @Override
+    public boolean waitForElement(String selector, Boolean isVisible, Integer timeoutMs) {
+        try {
+            currentPage.locator(selector).waitFor(new Locator.WaitForOptions()
+                    .setState(isVisible ? WaitForSelectorState.VISIBLE : WaitForSelectorState.ATTACHED)
+                    .setTimeout(timeoutMs));
+        } catch (TimeoutError e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean waitForPage(String page, Integer timeoutMs) {
+        try {
+            currentPage.waitForURL(page, new Page.WaitForURLOptions().setTimeout(timeoutMs));
+        } catch (TimeoutError e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void reload() {
+        currentPage.reload();
     }
 }
