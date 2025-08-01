bb91e6ec2ae0e0cab23fe5b75faf411ad42795fd
Alexandre Patelli <alexandre.patelli@outlook.com>
2025-08-01 10:47:43 +0200

    feat(HttpUtils): add methods for mocking and verifying HTTP requests (#615)

--- tzatziki-http/src/main/java/com/decathlon/tzatziki/utils/HttpUtils.java
+++ tzatziki-http/src/main/java/com/decathlon/tzatziki/utils/HttpUtils.java
@@ -4,10 +4,13 @@ import com.decathlon.tzatziki.steps.HttpSteps;
 import com.github.tomakehurst.wiremock.client.MappingBuilder;
 import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
 
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
 import static com.decathlon.tzatziki.steps.HttpSteps.MOCKED_PATHS;
 import static com.decathlon.tzatziki.steps.HttpSteps.wireMockServer;
+import static com.decathlon.tzatziki.utils.Comparison.CONTAINS;
 
 public class HttpUtils {
     public static String url() {
@@ -26,6 +29,27 @@ public class HttpUtils {
 
         MappingBuilder request = interaction.request.toMappingBuilder(null, HttpWiremockUtils.match(interaction.request.path), comparison).willReturn(responseDefinition);
         wireMockServer.stubFor(request);
+    }
+
+    public static void mockSimpleRequest(String path, Method method, String status, String responseBody) {
+        Interaction.Response.ResponseBuilder responseBuilder = Interaction.Response.builder().status(status);
+
+        if (responseBody != null) {
+            responseBuilder
+                    .headers(Map.of("Content-Type", "application/json"))
+                    .body(Interaction.Body.builder().payload(responseBody).build());
+        }
+
+        Interaction interaction = Interaction.builder()
+                .request(Interaction.Request.builder().path(path).method(method).build())
+                .response(List.of(responseBuilder.build()))
+                .build();
+
+        HttpUtils.mockInteraction(interaction, Comparison.CONTAINS, null);
+    }
+
+    public static void verify(Interaction.Request request, Integer times) {
+        HttpSteps.assertHasReceived(request.toRequestPatternBuilder(null, HttpWiremockUtils.match(request.path), CONTAINS), times);
     }
 
     public static void reset() {
